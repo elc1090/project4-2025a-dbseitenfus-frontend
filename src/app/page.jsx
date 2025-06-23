d9f6799a9e2dabe6f901dbad1ce5c7816e2d1acd
Daniel Seitenfus <dbseitenfus@inf.ufsm.br>
2025-06-22 21:46:40 -0300

    fix: login page flashing problem when accessing the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,35 +7,46 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 import DocumentCard from "@/components/document-card"
 import { getDocuments, createDocument } from "@/lib/api"
 import { useSession } from "next-auth/react"
+import { CircularProgress } from "@/components/ui/circular-progress"
 
 export default function Home() {
-
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
   const router = useRouter();
-  const { data: session } =  useSession() 
+  const { data: session, status } = useSession()
 
   useEffect(() => {
-    if(!session) {
-      router.push('/login'); 
+    if (status === "loading") {
+      return; 
     }
-  }, []);
+    if (!session) {
+      router.push('/login');
+    }
+    setIsAuthenticating(false);
+  }, [status, session]);
 
   const [documents, setDocuments] = useState([])
   const [user, setUser] = useState([])
 
   useEffect(() => {
     async function fetchUser() {
+      if (status != "authenticated" || !session || !session.user) {
+        return;
+      }
       setUser(session.user)
     }
     fetchUser()
-  }, [])
+  }, [status, session])
 
   useEffect(() => {
     async function fetchData() {
+      if (!session || !session.user || status === "loading") {
+        return;
+      }
       const documents = await getDocuments(session.accessToken)
       setDocuments(documents)
     }
     fetchData()
-  }, [])
+  }, [status, session])
 
   async function onNewDocumentButtonClicked() {
     const newDocument = {
@@ -47,27 +58,34 @@ export default function Home() {
     setDocuments((prevDocuments) => [...prevDocuments, documentResponse])
   }
 
-  return (
-    <div className="[--header-height:calc(theme(spacing.14))]">
-      <SidebarProvider className="flex flex-col">
-        <SiteHeader />
-        <div className="flex flex-1">
-          <AppSidebar onNewDocumentButtonClicked={onNewDocumentButtonClicked} user={user} />
-          <SidebarInset>
-            {documents.length === 0 ? (
-              <div className="flex flex-col items-center justify-center h-full">
-                <h2 className="text-2xl font-bold">Nenhum documento encontrado</h2>
-                <p className="text-gray-500">Clique no botão "Novo" e crie um documento.</p>
-              </div>
-            ) : (
-              <div className="grid grid-cols-4 gap-4 p-3">
-                {documents.map((document) => (
-                  <DocumentCard key={document.id} document={document} />
-                ))}
-              </div>)}
-          </SidebarInset>
-        </div>
-      </SidebarProvider>
+  if (!isAuthenticating) {
+    return (
+
+      <div className="[--header-height:calc(theme(spacing.14))]">
+        <SidebarProvider className="flex flex-col">
+          <SiteHeader />
+          <div className="flex flex-1">
+            <AppSidebar onNewDocumentButtonClicked={onNewDocumentButtonClicked} user={user} />
+            <SidebarInset>
+              {documents.length === 0 ? (
+                <div className="flex flex-col items-center justify-center h-full">
+                  <h2 className="text-2xl font-bold">Nenhum documento encontrado</h2>
+                  <p className="text-gray-500">Clique no botão "Novo" e crie um documento.</p>
+                </div>
+              ) : (
+                <div className="grid grid-cols-4 gap-4 p-3">
+                  {documents.map((document) => (
+                    <DocumentCard key={document.id} document={document} />
+                  ))}
+                </div>)}
+            </SidebarInset>
+          </div>
+        </SidebarProvider>
+      </div>
+    )
+  } else {
+    <div className="flex items-center justify-center h-screen">
+      <CircularProgress />
     </div>
-  )
+  }
 }
